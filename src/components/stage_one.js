3297ac0d04061c2429490a5368a2cde2222b2862
rugieri <rugieri.ronaldo@gmail.com>
2021-03-30 10:57:57 -0300

    update

---
+++ src/components/stage_one.js
@@ -0,0 +1,64 @@
+import React, { useContext } from 'react';
+import { StyleSheet, View } from 'react-native';
+
+import {Formik} from 'formik';
+import * as Yup from 'yup';
+import { Input, Button, ListItem, Text } from 'react-native-elements';
+
+import { MyContext } from '../context';
+
+const StageOne = () => {
+    const context = useContext(MyContext);
+
+    console.log(context)
+
+    return(
+        <Formik
+            initialValues={{player:''}}
+            validationSchema={Yup.object({
+                player:Yup.string()
+                .min(3, 'Must be more than 3 characters')
+                .max(20, 'Must be less than 20 characters')
+                .required('Sorry, you must put a name')
+
+            })}
+            onSubmit={(values, {resetForm})=>{
+                alert(values)
+
+            }}
+
+        >    
+            {({ handleChange, handleBlur, handleSubmit,values,touched,errors})=>(
+                <>
+                    <Text>Who pays the Bill</Text>
+                    <Input
+                        placeholder='Add names here'
+                        leftIcon={{type:'antdesign', name:'adduser'}}
+                        inputContainerStyle={{
+                            marginHorizontal:50,
+                            marginTop:50
+                        }}
+
+                        renderErrorMessage={errors.player && touched}
+                        errorMessage={errors.player}
+                        errorStyle={{
+                            marginHorizontal:50
+                        }}
+
+                        onChangeText={handleChange('player')}
+                        onBlur={handleBlur('player')}
+                        value={values.player}
+                    />
+
+                    <Button
+                        buttonStyle={styles.button}
+                        title="Add player"
+                        onPress={handleSubmit}
+                    />    
+                </>    
+            )}
+        </Formik>
+    )
+}
+
+export default StageOne;
---
+++ src/components/stage_two.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const StageTwo = () => {
+
+    return(
+        <View>
+        <Text>Stage Two</Text>
+        </View>
+    )
+}
+
+export default StageTwo;
